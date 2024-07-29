22acb21a09f767843e65e298f2eec15163052faa
Martijn van Bloois <mvbloois@gmail.com>
2024-07-29 14:32:30 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# Configuration parameters
+DOWNLOAD_URL = 'https://www.ema.europa.eu/en/documents/product-information/product-information-urls-member-states_en.xls'
+OUTPUT_PATH = 'product-information-urls-member-states_en.xls'
+CSV_DIRECTORY = '/csv_documents'
